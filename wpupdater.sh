03f242aea3e4a0ee9de7239a013392b94bdc2a66
Derek Moon <dmoon@forumone.com>
2017-02-21 16:54:28 -0800

    update all default themes

--- wpupdater.sh
+++ wpupdater.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 clear
+NOW=$(date +"%m-%d-%Y")
 echo "Hello $USER"
 #How to store bash to array
 #http://www.linuxquestions.org/questions/linux-general-1/store-multi-line-output-into-an-array-in-a-linux-bash-script-706878/
-echo -n "Enter the ticket ID: "
-read ticket
-echo -n "Enter Month: "
-read -n 3 month
+echo $NOW
+read date
+#echo -n "Enter Month: "
+#read -n 3 month
 echo
 
 #Create a new branch, from LIVE
-git checkout live
 git pull
-git checkout -b $ticket-$month-patching
+git checkout -b $date-patching
 
 #Returns array of plugins needing updates
 WP_LIST=( $(wp plugin list --field=name --update=available))
@@ -26,20 +26,20 @@ i="0"
 
 while [ $i -lt $UPDATE_COUNT ]
 	do
-		wp plugin update ${WP_LIST[$i]} 
-		git add --all 
+		wp plugin update ${WP_LIST[$i]}
+		git add --all
 		git commit -m "update plugin ${WP_LIST[$i]}"
-		i=$(( $i + 1 )) 
-done	
+		i=$(( $i + 1 ))
+done
 
 #Update Factory Themes
 wp theme update --all
-git add -A 
+git add -A
 git commit -m "update all default themes"
 
 #Update to latest WP Core Version
 wp core update
-git add --all 
+git add --all
 git commit -m "update WP Core to latest version"
 
 echo "WPupdater script is now complete!"
